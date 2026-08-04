652d5f41b3d2251906d1d4ea82b209d700452110
Manan A. Shah <50628077+mananashah007@users.noreply.github.com>
2026-08-05 00:25:05 +0530

    Enhance app.py with database integration and ticket display
    Updated Streamlit app to include database connection and display tickets.

--- app.py
+++ app.py
@@ -1,5 +1,26 @@
 import streamlit as st
+from database import Database
 
-st.set_page_config(page_title = 'Support Ticket System')
-st.title('Welcome to Support Ticket System')
-st.success('Deployement Successfull!')
+st.set_page_config(
+    page_title="Support Ticket System",
+    layout="wide"
+)
+
+st.title("Welcome to Support Ticket System")
+
+try:
+
+    db = Database()
+
+    tickets = db.get_all_tickets()
+
+    st.success("Connected to Lakebase!")
+
+    st.dataframe(
+        tickets,
+        use_container_width=True
+    )
+
+except Exception as e:
+
+    st.error(str(e))
